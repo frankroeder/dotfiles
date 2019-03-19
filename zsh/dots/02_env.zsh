c2fc43878b178f578d0290a49eb8ef0a6b6268ad
Frank Roeder <frank@peaksandpies.com>
2019-03-19 16:50:26 +0100

    Minor fixes

--- zsh/dots/02_env.zsh
+++ zsh/dots/02_env.zsh
@@ -23,15 +23,15 @@ export FZF_DEFAULT_COMMAND="ag -g '' --path-to-ignore ${DOTFILES}/ignore"
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-export FZF_DEFAULT_OPTS="--height=50% --reverse --no-mouse"
+export FZF_DEFAULT_OPTS="--height=50% --reverse"
 
-export FZF_CTRL_T_OPTS="--preview 'file {}' --preview-window down:1:hidden:wrap 
+export FZF_CTRL_T_OPTS="--no-mouse --preview 'file {}' --preview-window down:1:hidden:wrap 
   --bind '?:toggle-preview'"
 
-export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap
+export FZF_CTRL_R_OPTS="--no-mouse --preview 'echo {}' --preview-window down:3:hidden:wrap
   --bind '?:toggle-preview'"
 
-export FZF_ALT_C_OPTS="--exit-0 --preview 'tree -C {} | head -200'"
+export FZF_ALT_C_OPTS="--no-mouse --exit-0 --preview 'tree -C {} | head -200'"
 
 # HOMEBREW
 export HOMEBREW_NO_INSECURE_REDIRECT=1
