e30d07f3772c3882a64bab70cafda4f91fe6cdf4
Frank Roeder <frankyroeder@icloud.com>
2022-01-12 23:22:08 +0100

    Add script to toggle applications with skhd

---
+++ scripts/toggle_app.bash
@@ -0,0 +1,9 @@
+# toggle the visibility of an application
+main(){
+  if [[ $(osascript -e 'set front_app to (path to frontmost application as Unicode text)' | grep "$@") ]]; then
+    osascript -e "tell application \"System Events\" to set visible of process \"$@\" to false"
+  else
+    open "/Applications/$@.app";
+  fi
+}
+main "$@";
