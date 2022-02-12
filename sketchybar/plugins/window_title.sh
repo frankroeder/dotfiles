6f3c81b7dcf6914b807b0a0e00d7344308516c70
Frank Roeder <frankyroeder@icloud.com>
2022-02-12 23:00:27 +0100

    Update yabai and sketchybar settings

--- sketchybar/plugins/window_title.sh
+++ sketchybar/plugins/window_title.sh
@@ -7,10 +7,4 @@ if [[ $WINDOW_TITLE = "" ]]; then
   WINDOW_TITLE=$(/opt/homebrew/bin/yabai -m query --windows --window | jq -r '.app')
 fi
 
-if [[ ${#WINDOW_TITLE} -gt 50 ]]; then
-  WINDOW_TITLE=$(echo "$WINDOW_TITLE" | cut -c 1-50)
-  sketchybar -m --set title label="│ $WINDOW_TITLE"…
-  exit 0
-fi
-
-sketchybar -m --set title label="│ $WINDOW_TITLE"
+sketchybar -m --set title label="│ $WINDOW_TITLE |"
---
+++ sketchybar/plugins/yabai_spaces.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+STATIC_NAMES=("" "I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX" "X" "XI" "XII" "XIII" "XIV" "XV") #0 to 15
+args=()
+QUERY="$(yabai -m query --spaces | jq -r '.[] | [.index, .windows[0], .label, .display, .visible] | @sh')"
+NAMES=""
+COUNT=0
+
+# Read the current spaces from the yabai query
+while read -r index window yabai_name display visible
+do
+  COUNT=$((COUNT+1))
+  NAME="$(echo "${yabai_name}" | tr -d "'")"
+  if [ "${window}" = "null" ]; then
+    label="${STATIC_NAMES[${index}]}"
+  else
+    label="${STATIC_NAMES[${index}]}°"
+  fi
+  if [ "$NAME" = "" ] || [ "$NAME" = " " ]; then
+    NAME="${index}"
+  fi
+
+  NAMES="$NAMES $NAME"
+  args+=(--clone "$NAME" space_template after \
+         --set "$NAME" label="${label}" \
+                       label.highlight_color="0xff6ed6e6" \
+                       associated_display=${display} \
+                       label.highlight=${visible} \
+                       drawing=on)
+done <<< "$QUERY"
+
+# Reorder them and stick them onto sketchybar
+args+=(--reorder $NAMES)
+sketchybar -m ${args[@]} &> /dev/null
+
+# Get the current space count
+CURRENT_SPACE_COUNT=$(sketchybar -m --query bar | jq '.items | map(try tonumber catch 0) | max')
+
+# If our current space count is greater than the count found by the yabai query,
+# go through and remove the destroyed spaces
+while [ $CURRENT_SPACE_COUNT -gt $COUNT ]
+do
+  sketchybar -m --remove "${CURRENT_SPACE_COUNT}"
+  CURRENT_SPACE_COUNT=$((CURRENT_SPACE_COUNT-1))
+done
