6138ffd9c09b0215a340005781482ca51717a290
Frank Roeder <fr.coding@proton.me>
2022-09-30 08:52:40 +0200

    Show vimtex compiler status in lualine

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,8 +1,41 @@
+local compile_status = function()
+  local vimtex = vim.b.vimtex
+  local compiler_status = vimtex.compiler.status
+  -- not started or stopped
+  if compiler_status == -1 or compiler_status == 0 then
+    return ""
+  end
+  if vim.b.vimtex["compiler"]["continuous"] == 1 then
+    -- running
+    if compiler_status == 1 then
+      return "{⋯}"
+    -- success
+    elseif compiler_status == 2 then
+      return "{✓}"
+    -- failed
+    elseif compiler_status == 3 then
+      return "{✗}"
+    end
+  end
+  return ""
+end
+
 require("lualine").setup {
   options = {
     globalstatus = true,
     theme = "catppuccin",
     disabled_filetypes = { "help" },
+  },
+  sections = {
+    lualine_x = {
+      {
+        compile_status,
+        cond = function()
+          return vim.bo.filetype == "tex"
+        end,
+      },
+      "filetype",
+    },
   },
   extensions = { "nvim-tree", "fzf" },
 }
