190993dc12260ee4709e07f4bd50dc2ba31f6157
Frank Roeder <fr.coding@proton.me>
2023-06-30 22:31:19 +0200

    Refactor sketchybar

--- sketchybar/items/left.sh
+++ sketchybar/items/left.sh
@@ -2,40 +2,40 @@
 ############## LEFT ITEMS ##############
 
 # yabai space number
-sketchybar -m --add item yabai_spaces left                                            \
-              --set yabai_spaces drawing=off                                          \
-                                 updates=on                                           \
-                                 script="$PLUGIN_DIR/yabai_spaces.sh"                 \
-              --subscribe yabai_spaces space_change window_created window_destroyed   \
-                                                                                      \
-              --add item space_template left                                          \
-              --set      space_template icon.highlight_color=0xff9dd274               \
-                                 label.font="$FONT:Bold:14"                           \
-                                 drawing=off                                          \
-                                 lazy=off
+sketchybar -m --add item yabai_spaces left \
+              --set yabai_spaces drawing=off \
+                    updates=on \
+                    script="$PLUGIN_DIR/yabai_spaces.sh" \
+              --subscribe yabai_spaces space_change window_created window_destroyed \
+              \
+              --add item space_template left \
+              --set space_template icon.highlight_color=0xff9dd274 \
+                    label.font="$FONT:Bold:14" \
+                    drawing=off \
+                    lazy=off
 
-sketchybar -m --add item yabai_layout left                                            \
-              --set yabai_layout script="$PLUGIN_DIR/yabai_layout.sh"                 \
-                    lazy=off                                                          \
-              --subscribe yabai_layout front_app_switched window_focus layout_change  \
-                                                                                      \
-              --add item yabai_property left                                          \
-              --set yabai_property script="$PLUGIN_DIR/yabai_property.sh"             \
-                    label.font="$FONT:Bold:12"                                        \
-                    lazy=off                                                          \
+sketchybar -m --add item yabai_layout left \
+              --set yabai_layout script="$PLUGIN_DIR/yabai_layout.sh" \
+                    lazy=off \
+              --subscribe yabai_layout front_app_switched window_focus layout_change \
+              \
+              --add item yabai_property left \
+              --set yabai_property script="$PLUGIN_DIR/yabai_property.sh" \
+                    label.font="$FONT:Bold:12" \
+                    lazy=off \
               --subscribe yabai_property front_app_switched window_focus property_change
 
-sketchybar -m --add item space_separator left                                         \
-              --set space_separator  icon="|"                                         \
-                                     icon.padding_left=15                             \
-                                     label.padding_right=15                           \
-                                     icon.font="$FONT:Bold:15.0"                      \
-              --add item mail left                                                    \
-              --set mail associated_space=1,2,3                                       \
-                      update_freq=30                                                  \
-                      script="$PLUGIN_DIR/mail.sh"                                    \
-                      icon.font="$FONT:Bold:18.0"                                     \
-                      icon=
+sketchybar -m --add item space_separator left \
+              --set space_separator icon="|" \
+                    icon.padding_left=15 \
+                    label.padding_right=15 \
+                    icon.font="$FONT:Bold:15.0" \
+              --add item mail left \
+              --set mail associated_space=1,2,3 \
+                    update_freq=30 \
+                    script="$PLUGIN_DIR/mail.sh" \
+                    icon.font="$FONT:Bold:18.0" \
+                    icon=
 
 # show current VPN connection name
 sketchybar -m --add item vpn left \
--- sketchybar/items/right.sh
+++ sketchybar/items/right.sh
@@ -1,71 +1,69 @@
 #!/usr/bin/env bash
 ############## RIGHT ITEMS ##############
 
-sketchybar -m --add item topproc right                            \
-              --set topproc      update_freq=5                    \
-                                 icon.drawing=off                 \
-                                 width=0                          \
-                                 y_offset=6                       \
-                                 label.font="$FONT:Bold:10.0"     \
-                                 script="$PLUGIN_DIR/topproc.sh"  \
-                                 lazy=on                          \
-                                                                  \
-              --add       item cpu_percent right                  \
-              --set       cpu_percent label.font="$FONT:Bold:12"  \
-                                      y_offset=-4                 \
-                                      width=40                    \
-                                      icon.drawing=off            \
-                                      update_freq=2               \
-                                      lazy=on                     \
-                                                                        \
-              --add graph cpu_user right 150                            \
-              --set cpu_user     graph.color=0xffe1e3e4                 \
-                                 update_freq=2                          \
-                                 width=0                                \
-                                 script="$PLUGIN_DIR/cpu_graph.sh"      \
-                                 label.drawing=off                      \
-                                 icon.drawing=off                       \
-                                 background.height=23                   \
-                                 background.color=0x00000000            \
-                                 lazy=on                                \
-                                                                        \
-              --add graph cpu_sys right 150                             \
-              --set cpu_sys graph.color=0xff9dd274                      \
-                                  label.drawing=off                     \
-                                  icon.drawing=off                      \
-                                  background.height=23                  \
-                                  background.color=0x00000000           \
-                                  background.border_color=0x00000000    \
-              --add bracket       cpu                                   \
-                                  cpu_separator                         \
-                                  cpu_topproc                           \
-                                  cpu_percent                           \
-                                  cpu_user                              \
-                                  cpu_sys                               \
-                                                                        \
-              --set cpu           background.drawing=on
+sketchybar -m --add item topproc right \
+              --set topproc update_freq=5 \
+                    icon.drawing=off \
+                    width=0 \
+                    y_offset=6 \
+                    label.font="$FONT:Bold:10.0" \
+                    script="$PLUGIN_DIR/topproc.sh" \
+                    lazy=on \
+              \
+              --add item cpu_percent right \
+              --set cpu_percent label.font="$FONT:Bold:12" \
+                    y_offset=-4 \
+                    width=40 \
+                    icon.drawing=off \
+                    update_freq=2 \
+                    lazy=on \
+              \
+              --add graph cpu_user right 150 \
+              --set cpu_user graph.color=0xffe1e3e4 \
+                    update_freq=2 \
+                    width=0 \
+                    script="$PLUGIN_DIR/cpu_graph.sh" \
+                    label.drawing=off \
+                    icon.drawing=off \
+                    background.height=23 \
+                    background.color=0x00000000 \
+                    lazy=on \
+              \
+              --add graph cpu_sys right 150 \
+              --set cpu_sys graph.color=0xff9dd274 \
+                    label.drawing=off \
+                    icon.drawing=off \
+                    background.height=23 \
+                    background.color=0x00000000 \
+                    background.border_color=0x00000000 \
+              --add bracket cpu \
+                    cpu_separator \
+                    cpu_topproc \
+                    cpu_percent \
+                    cpu_user \
+                    cpu_sys \
+              --set cpu background.drawing=on
 
 # network stats
-sketchybar -m --add item topmem right                                  \
-              --set topmem      update_freq=5                          \
-                                script="$PLUGIN_DIR/topmem.sh"         \
-                                label.font="$FONT:Bold:12.0"           \
-                                lazy=on                                \
-                                                                       \
-              --add item network_up right                              \
-              --set network_up label.font="$FONT:Bold:9"               \
-                                  icon.font="$FONT:Bold:12"            \
-                                  icon=↑                               \
-                                  icon.highlight_color=0xff8b0a0d      \
-                                  y_offset=5                           \
-                                  width=0                              \
-                                  update_freq=2                        \
-                                  script="$PLUGIN_DIR/network.sh"      \
-                                                                       \
-              --add item network_down right                            \
-              --set network_down label.font="$FONT:Bold:9"             \
-                                      icon.font="$FONT:Bold:12"        \
-                                      icon=↓                           \
-                                      icon.highlight_color=0xff10528c  \
-                                      y_offset=-5
-
+sketchybar -m --add item topmem right \
+              --set topmem update_freq=5 \
+                    script="$PLUGIN_DIR/topmem.sh" \
+                    label.font="$FONT:Bold:12.0" \
+                    lazy=on \
+              \
+              --add item network_up right \
+              --set network_up label.font="$FONT:Bold:9" \
+                    icon.font="$FONT:Bold:12" \
+                    icon=↑ \
+                    icon.highlight_color=0xff8b0a0d \
+                    y_offset=5 \
+                    width=0 \
+                    update_freq=2 \
+                    script="$PLUGIN_DIR/network.sh" \
+              \
+              --add item network_down right \
+              --set network_down label.font="$FONT:Bold:9" \
+                    icon.font="$FONT:Bold:12" \
+                    icon=↓ \
+                    icon.highlight_color=0xff10528c \
+                    y_offset=-5
