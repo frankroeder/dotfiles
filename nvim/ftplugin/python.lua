203c525a2b20116f5ad2d1198820d39c446a58d9
Frank Roeder <fr.coding@proton.me>
2023-09-27 05:35:56 +0200

    Add new yabai window rules
    - add nvim python options

---
+++ nvim/ftplugin/python.lua
@@ -0,0 +1,6 @@
+vim.cmd.inoreabbrev("<buffer> true True")
+vim.cmd.inoreabbrev("<buffer> false False")
+
+vim.cmd.inoreabbrev("<buffer> null None")
+vim.cmd.inoreabbrev("<buffer> none None")
+vim.cmd.inoreabbrev("<buffer> nil None")
