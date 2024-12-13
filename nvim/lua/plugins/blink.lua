8b1020c542c825a6bb22e567df5ec10c93a243d3
Frank Roeder <fr.coding@proton.me>
2024-12-13 17:08:41 +0100

    Update blink.cmp and remove avante + deps

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -20,18 +20,15 @@ return {
     },
 
     snippets = {
-      -- Function to use when expanding LSP provided snippets
       expand = function(snippet)
         require("luasnip").lsp_expand(snippet)
       end,
-      -- -- Function to use when checking if a snippet is active
       active = function(filter)
         if filter and filter.direction then
           return require("luasnip").jumpable(filter.direction)
         end
         return require("luasnip").in_snippet()
       end,
-      -- Function to use when jumping between tab stops in a snippet, where direction can be negative or positive
       jump = function(direction)
         require("luasnip").jump(direction)
       end,
@@ -41,36 +38,12 @@ return {
       keyword = {
         range = "full",
       },
-      trigger = {
-        -- When false, will not show the completion window automatically when in a snippet
-        show_in_snippet = true,
-        -- When true, will show the completion window after typing a character that matches the `keyword.regex`
-        show_on_keyword = true,
-        -- When true, will show the completion window after typing a trigger character
-        show_on_trigger_character = true,
-        -- LSPs can indicate when to show the completion window via trigger characters
-        -- however, some LSPs (i.e. tsserver) return characters that would essentially
-        -- always show the window. We block these by default.
-        show_on_blocked_trigger_characters = { " ", "\n", "\t" },
-        -- When both this and show_on_trigger_character are true, will show the completion window
-        -- when the cursor comes after a trigger character after accepting an item
-        show_on_accept_on_trigger_character = true,
-        -- When both this and show_on_trigger_character are true, will show the completion window
-        -- when the cursor comes after a trigger character when entering insert mode
-        show_on_insert_on_trigger_character = true,
-        -- List of trigger characters (on top of `show_on_blocked_trigger_characters`) that won't trigger
-        -- the completion window when the cursor comes after a trigger character when
-        -- entering insert mode/accepting an item
-        show_on_x_blocked_trigger_characters = { "'", '"', "(" },
-      },
       list = {
-        -- Maximum number of items to display
         max_items = 100,
         selection = "auto_insert",
       },
       accept = {
         auto_brackets = {
-          -- Whether to auto-insert brackets for functions
           enabled = true,
         },
       },
@@ -82,7 +55,6 @@ return {
       },
 
       documentation = {
-        -- Controls whether the documentation window will automatically show when selecting a completion item
         auto_show = true,
         window = {
           border = "rounded",
@@ -98,22 +70,26 @@ return {
     },
 
     fuzzy = {
-      -- when enabled, allows for a number of typos relative to the length of the query
-      -- disabling this matches the behavior of fzf
       use_typo_resistance = false,
-      max_items = 100,
     },
 
     sources = {
-      completion = {
-        enabled_providers = function(ctx)
-          if vim.bo.filetype == "tex" then
-            return { "buffer", "vimtex", "luasnip" }
-          else
-            return { "lsp", "luasnip", "path", "buffer", "lazydev" }
-          end
-        end,
-      },
+      default = function()
+        local node = vim.treesitter.get_node()
+        if vim.bo.filetype == "tex" then
+          return { "buffer", "vimtex", "luasnip" }
+        elseif
+          node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type())
+        then
+          return { "buffer" }
+        else
+          return { "lsp", "luasnip", "path", "buffer", "lazydev" }
+        end
+      end,
+      cmdline = {},
+      min_keyword_length = function()
+        return vim.tbl_contains({ "markdown", "tex", "text" }, vim.bo.filetype) and 2 or 0
+      end,
       providers = {
         vimtex = {
           name = "vimtex",
@@ -134,7 +110,7 @@ return {
         },
         lsp = {
           min_keyword_length = 2,
-          fallback_for = { "lazydev" },
+          fallbacks = { "lazydev" },
         },
         lazydev = {
           name = "LazyDev",
