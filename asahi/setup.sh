417f90c8ca80ec2eb10f2b3dce51c919f24f86a9
Frank Roeder <fr.coding@proton.me>
2026-01-05 15:45:44 +0100

    Asahi vanilla setup script

---
+++ asahi/setup.sh
@@ -0,0 +1,22 @@
+DOTFILES="$HOME/.dotfiles"
+sudo dnf upgrade;
+curl -fsSL https://install.danklinux.com | sh;
+
+mkdir -p $HOME/config
+mkdir -p $HOME/.zsh
+mkdir -p $HOME/tmp
+mkdir -p $HOME/.Trash
+mkdir -p $HOME/Downloads
+mkdir -p $HOME/bin
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --bin; \
+# nvim
+sudo dnf install neovim uv cargo;
+touch $HOME/.localnvim.lua;
+ln -sfv $DOTFILES/nvim $HOME/.config;
+
+ln -sfv $DOTFILES/zsh/zshrc $HOME/.zshrc;
+ln -sfv $DOTFILES/zsh/zlogin $HOME/.zlogin;
+ln -sfv $DOTFILES/zsh/zshenv $HOME/.zshenv;
+ln -sfv $DOTFILES/zsh/zprofile $HOME/.zprofile;
+source $HOME/.zshrc
