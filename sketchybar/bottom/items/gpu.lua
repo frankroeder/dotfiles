fd185fd48fddf4db8b80d973d83169de9c54be61
Frank Roeder <fr.coding@proton.me>
2026-01-12 08:58:05 +0100

    Add GPU temperature

--- sketchybar/bottom/items/gpu.lua
+++ sketchybar/bottom/items/gpu.lua
@@ -11,22 +11,27 @@ local gpu = sbar.add('graph', 'widgets.gpu', 80, {
     y_offset = 2,
 	},
   label = {
-    string = 'GPU ??%',
+    string = 'GPU ??% ??°C',
     font = {
       size = 10.0,
     },
     align = "right",
     width = 0,
-    padding_right = 18,
+    padding_right = 8,
     y_offset = 8,
 	},
   update_freq = 2,
 })
 
 gpu:subscribe('routine', function(env)
-  sbar.exec('macmon pipe -s 1 | jq -r \'(.gpu_usage[1] * 100 | floor)\'', function(output)
-    local load = tonumber(output)
-    if load then
+  sbar.exec('macmon pipe -s 1 | jq -r \'[(.gpu_usage[1] * 100 | floor), .temp.gpu_temp_avg] | join(",")\'' , function(output)
+    local parts = {}
+    for part in string.gmatch(output, '([^,]+)') do
+      table.insert(parts, part)
+    end
+    local load = tonumber(parts[1])
+    local temp = tonumber(parts[2])
+    if load and temp then
       gpu:push({ load / 100. })
 
       local color = colors.blue
@@ -42,7 +47,7 @@ gpu:subscribe('routine', function(env)
 
       gpu:set({
         graph = { color = color },
-        label = "GPU " .. load .. '%',
+        label = "GPU " .. load .. '% ' .. math.floor(temp) .. '°C',
       })
     end
   end)
