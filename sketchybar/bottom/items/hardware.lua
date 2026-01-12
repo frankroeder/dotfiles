b9aadb97d001a6051f24d78bf13fcb48343616b8
Frank Roeder <fr.coding@proton.me>
2026-01-12 15:51:05 +0100

    Two line plot CPU

--- sketchybar/bottom/items/hardware.lua
+++ sketchybar/bottom/items/hardware.lua
@@ -68,6 +68,16 @@ local cpu = sbar.add("graph", "widgets.cpu", 138, {
   update_freq = 2,
 })
 
+local ecpu = sbar.add("graph", "widgets.ecpu", 138, {
+  position = "right",
+  graph = { color = colors.green },
+  background = { drawing = false },
+  icon = { drawing = false },
+  label = { drawing = false },
+  padding_right = -163,
+  y_offset = 4,
+})
+
 local ram_popup = sbar.add("item", {
   position = "popup." .. ram_g.name,
   label = {
@@ -91,13 +101,14 @@ local power = sbar.add('item', 'widgets.power', {
     },
     padding_right = 2,
   },
+  padding_right = 20,
 })
 
 cpu:subscribe('routine', function(env)
   sbar.exec('/opt/homebrew/bin/macmon pipe -s 1', function(output)
     if not output or not output.ecpu_usage or not output.pcpu_usage or not output.temp or not output.memory or not output.gpu_usage then return end
-    local ecpu = math.floor(output.ecpu_usage[2] * 100)
-    local pcpu = math.floor(output.pcpu_usage[2] * 100)
+    local ecpu_val = math.floor(output.ecpu_usage[2] * 100)
+    local pcpu_val = math.floor(output.pcpu_usage[2] * 100)
     local cpu_temp = output.temp.cpu_temp_avg
     local ram_total = output.memory.ram_total
     local ram_used = output.memory.ram_usage
@@ -106,25 +117,15 @@ cpu:subscribe('routine', function(env)
     local gpu_used = math.floor(output.gpu_usage[2] * 100)
     local gpu_temp = output.temp.gpu_temp_avg
 
-    if ecpu and pcpu and cpu_temp and ram_total and ram_used and swap_total and swap_used and gpu_used and gpu_temp then
+    if ecpu_val and pcpu_val and cpu_temp and ram_total and ram_used and swap_total and swap_used and gpu_used and gpu_temp then
 
       -- Update CPU
-      local cpu_total = (ecpu + pcpu) / 2
-      local color = colors.blue
-      if cpu_total > 30 then
-        if cpu_total < 60 then
-          color = colors.yellow
-        elseif cpu_total < 80 then
-          color = colors.orange
-        else
-          color = colors.red
-        end
-      end
       cpu:set({
-        graph = { color = color },
-        label = "eCPU " .. ecpu .. "% pCPU " .. pcpu .. "% " .. math.floor(cpu_temp) .. "°C",
+        graph = { color = colors.blue },
+        label = "eCPU " .. ecpu_val .. "% pCPU " .. pcpu_val .. "% " .. math.floor(cpu_temp) .. "°C",
       })
-      cpu:push({ cpu_total / 100. })
+      cpu:push({ pcpu_val / 100. })
+      ecpu:push({ ecpu_val / 100. })
 
       -- Update RAM
       local ram_pct = (ram_used / ram_total) * 100
