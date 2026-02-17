acae7b52a7b1ff609d0d4bec8c29a8acb18ae08c
Frank Röder <fr.coding@proton.me>
2026-02-17 06:09:06 +0100

    Update dms setup

--- asahi/setup.sh
+++ asahi/setup.sh
@@ -1,6 +1,7 @@
 DOTFILES="$HOME/.dotfiles"
 sudo dnf upgrade;
 curl -fsSL https://install.danklinux.com | sh;
+ln -sfv $DOTFILES/hypr $HOME/.config;
 
 mkdir -p $HOME/config
 mkdir -p $HOME/.zsh
@@ -9,9 +10,8 @@ mkdir -p $HOME/.Trash
 mkdir -p $HOME/Downloads
 mkdir -p $HOME/bin
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --bin; \
-# nvim
-sudo dnf install neovim uv cargo;
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --bin;
+sudo dnf install neovim uv cargo jq ffmpeg zsh ripgrep;
 touch $HOME/.localnvim.lua;
 ln -sfv $DOTFILES/nvim $HOME/.config;
 
@@ -19,4 +19,7 @@ ln -sfv $DOTFILES/zsh/zshrc $HOME/.zshrc;
 ln -sfv $DOTFILES/zsh/zlogin $HOME/.zlogin;
 ln -sfv $DOTFILES/zsh/zshenv $HOME/.zshenv;
 ln -sfv $DOTFILES/zsh/zprofile $HOME/.zprofile;
+chsh -s "$(command -v zsh)"
 source $HOME/.zshrc
+
+curl -fsSL https://claude.ai/install.sh | bash
