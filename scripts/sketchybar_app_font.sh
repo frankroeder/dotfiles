594e69cb047771a0ccf7cd1adcc31231890575a9
Frank Roeder <fr.coding@proton.me>
2026-02-26 07:22:02 +0100

    Update sketchybar_app_font stuff

--- scripts/sketchybar_app_font.sh
+++ scripts/sketchybar_app_font.sh
@@ -2,3 +2,4 @@
 asset_name="sketchybar-app-font.ttf"
 dest="$HOME/Library/Fonts/$asset_name"
 curl -fsSL "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/$asset_name" -o "$dest"
+curl -fsSL "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/icon_map.lua " -o "$DOTFILES/sketchybar/helpers/app_icons.lua"
