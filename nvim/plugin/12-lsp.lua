cbfed7b8aa146ce7600fdd2884b959077872095e
Frank Röder <fr.coding@proton.me>
2026-04-04 11:53:52 +0000

    Improve LspProgress autocommand error handling
    Refactor LspProgress autocommand to handle missing values.

--- nvim/plugin/12-lsp.lua
+++ nvim/plugin/12-lsp.lua
@@ -164,20 +164,6 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
   end,
 })
 
--- vim.api.nvim_create_autocmd("LspProgress", {
---   callback = function(ev)
---     vim.print(ev.data)
---     local value = ev.data.params.value
---     vim.api.nvim_echo({ { value.message or "done" } }, false, {
---       id = "lsp." .. ev.data.client_id,
---       kind = "progress",
---       source = "vim.lsp",
---       title = value.title,
---       status = value.kind ~= "end" and "running" or "success",
---       percent = value.percentage,
---     })
---   end,
--- })
 vim.api.nvim_create_autocmd("LspProgress", {
     callback = function(ev)
         local value = ev.data.params.value or {}
