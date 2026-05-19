cb0e49642da157e3c5974db51626007c56df76b2
Frank Roeder <fr.coding@proton.me>
2026-05-19 10:15:26 +0200

    Add monitors rule

--- asahi/hypr/conf.d/monitors.lua
+++ asahi/hypr/conf.d/monitors.lua
@@ -1,16 +1,8 @@
 hl.monitor({
-  output = "",
-  mode = "preferred",
-  position = "auto",
-  scale = 1.5,
-})
-
-hl.monitor({
   output = "eDP-1",
   mode = "3024x1890@120.000",
   position = "0x0",
   scale = 1.75,
-  vrr = 0,
 })
 
 hl.monitor({
@@ -18,8 +10,22 @@ hl.monitor({
   mode = "3840x2160@60.000",
   position = "-2560x-360",
   scale = 1.5,
-  vrr = 0,
+})
+
+hl.monitor({
+  output = "desc:Dell Inc. DELL P2723DE 895ZNR3",
+  mode = "2560x1440@59.95100",
+  position = "-160x-1252",
+  scale = 1.25,
+})
+
+hl.monitor({
+  output = "",
+  mode = "preferred",
+  position = "auto",
+  scale = 1.5,
 })
 
 hl.workspace_rule({ workspace = "1", monitor = "HDMI-A-1" })
 hl.workspace_rule({ workspace = "2", monitor = "HDMI-A-1" })
+hl.workspace_rule({ workspace = "3", monitor = "HDMI-A-1" })
